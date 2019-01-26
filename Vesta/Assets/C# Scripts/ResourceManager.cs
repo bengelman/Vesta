6aeaf8bce5738e8fd1fb26e26204b21733e1809d
bengelman <brendan.engelman@gmail.com>
2019-01-25 22:07:21 -0500

    Made some changes

---
+++ Vesta/Assets/C# Scripts/ResourceManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    public int player1Resources;
+    public int player2Resources;
+    public int player1Control;
+    public int player2Control;
+    public bool[][] tiles;
+    public Vector2[] occupiedTiles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Vector2 vec in occupiedTiles)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void AreTilesOccupied(Vector2[] tiles)
+    {
+
+    }
+    void SetTilesOccupied(Vector2[] tiles)
+    {
+
+    }
+}
